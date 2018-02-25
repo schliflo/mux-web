dc35c2435e9272668ffdd9950e113dcdf6f78f83
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2018-02-25 08:04:52 +0100

    fix paths

--- src/html.js
+++ src/html.js
@@ -29,7 +29,7 @@ module.exports = class HTML extends React.Component {
                     name='viewport'
                     content='width=device-width, initial-scale=1, shrink-to-fit=no'
                 />
-                <script src='/admin/netlify_redirect.js'/>
+                <script src='/admin/netlify_redirect.js' rel='preload'/>
                 {this.props.headComponents}
                 {css}
             </head>
