cf330d5f69c6a113e8adf2744f6d04c77d82944e
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2018-03-26 14:37:56 +0200

    google site verification

--- src/html.js
+++ src/html.js
@@ -26,6 +26,7 @@ module.exports = class HTML extends React.Component {
                 <meta charSet='utf-8'/>
                 <meta httpEquiv='x-ua-compatible' content='ie=edge'/>
                 <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'/>
+                <meta name='google-site-verification' content='Z7JIZc27nAoreMmaql-DJy7JNXwV8kI7zKXnsfSdm4s' />
                 <script src='https://identity.netlify.com/v1/netlify-identity-widget.js' rel='preload'/>
                 <script src='/admin/netlify_redirect.js' rel='preload'/>
                 {this.props.headComponents}
