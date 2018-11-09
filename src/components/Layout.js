4105fbd0bf3290ff501206ea7d70a45dddee716a
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2018-11-09 11:42:17 +0100

    add base layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,26 @@
-import React from 'react'
-import Helmet from 'react-helmet'
+import React from "react";
+import Helmet from "react-helmet";
+import { Link } from "gatsby";
 
-import Navbar from '../components/Navbar'
-import './scss/main.scss'
+import Navbar from "../components/Navbar";
+import "./scss/main.scss";
 
-const TemplateWrapper = ({children}) => (
-    <div>
-        <Helmet title="Max Neumeier - Video Editor"/>
-        <Navbar/>
-        <div>{children}</div>
-    </div>
+const TemplateWrapper = ({ children }) => (
+  <div id="page">
+    <Helmet title="Max Neumeier - Video Editor"/>
+    <Navbar/>
+    <div className="pageContent">{children}</div>
+    <footer id="footer" className="footer">
+      <span className="footer__copyright">&copy; 2018 Max Neumeier</span>
+      <nav id="nav-footer" className="footer__nav">
+        <ul>
+          <li>
+            <Link to='/imprint'>Imprint</Link>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  </div>
 );
 
-export default TemplateWrapper
+export default TemplateWrapper;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,97 @@
-import React from 'react'
-import {Link} from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import React from "react";
+import { Link } from "gatsby";
+import logo from "../img/logos/MN_Logo_white-02.svg";
 
-const Navbar = () => (
-    <nav className="navbar is-transparent">
-        <div className="container">
-            <div className="navbar-brand">
-                <Link to="/" className="navbar-item">
-                    <figure className="image">
-                        <img src={logo} alt="Kaldi" style={{width: '88px'}}/>
-                    </figure>
-                </Link>
-            </div>
-            <div className="navbar-start">
-                <Link className="navbar-item" to="/about">
-                    About
-                </Link>
-                <Link className="navbar-item" to="/products">
-                    Products
-                </Link>
-            </div>
-            <div className="navbar-end">
-                <a
-                    className="navbar-item"
-                    href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-          <span className="icon">
-            <img src={github} alt="Github"/>
-          </span>
-                </a>
-            </div>
+class Navbar extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      'menuOpen': false,
+    };
+  };
+
+  render() {
+    return (
+      <header id="header" className={`header ${this.state.menuOpen ? "nav-open" : ""}`}>
+        <Link to='/' className="header__logo">
+          <img src={logo} width="90" height="54" alt="Max Neumeier - Video Editor"/>
+        </Link>
+        <div className="nav-toggle" onClick={() => {
+          this.setState({
+            'menuOpen': !this.state.menuOpen,
+          });
+        }}>
+          <div className="nav-toggle__bar"/>
+          <div className="nav-toggle__bar"/>
+          <div className="nav-toggle__bar"/>
         </div>
-    </nav>
-);
+        <nav id="nav-main" className="header__nav">
+          <ul>
+            <li>
+              <Link to='/work' activeClassName='active' onClick={() => {
+                this.setState({
+                  'menuOpen': !this.state.menuOpen,
+                });
+              }}><span
+                data-text="Work">Work</span></Link>
+            </li>
+            <li>
+              <Link to='/about' activeClassName='active' onClick={() => {
+                this.setState({
+                  'menuOpen': !this.state.menuOpen,
+                });
+              }}><span
+                data-text="About">About</span></Link>
+            </li>
+            <li>
+              <Link to='/contact' activeClassName='active' onClick={() => {
+                this.setState({
+                  'menuOpen': !this.state.menuOpen,
+                });
+              }}><span
+                data-text="Contact">Contact</span></Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
 
-export default Navbar
+// const NavbarOld = () => (
+//   <nav className="navbar is-transparent">
+//     <div className="container">
+//       <div className="navbar-brand">
+//         <Link to="/" className="navbar-item">
+//           <figure className="image">
+//             <img src={logo} alt="Kaldi" style={{ width: "88px" }}/>
+//           </figure>
+//         </Link>
+//       </div>
+//       <div className="navbar-start">
+//         <Link className="navbar-item" to="/about">
+//           About
+//         </Link>
+//         <Link className="navbar-item" to="/products">
+//           Products
+//         </Link>
+//       </div>
+//       <div className="navbar-end">
+//         <a
+//           className="navbar-item"
+//           href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <span className="icon">
+//             <img src={github} alt="Github"/>
+//           </span>
+//         </a>
+//       </div>
+//     </div>
+//   </nav>
+// );
+
+export default Navbar;
