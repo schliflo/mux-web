c7235edb7ca8a6d7d1cd1a2381ac01e5fa20a51e
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2019-03-21 18:53:31 +0100

    fix netlify blog preview

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -2,14 +2,48 @@ import React from "react";
 import PropTypes from "prop-types";
 import { BlogPostTemplate } from "../../templates/blog-post";
 
-const BlogPostPreview = ({ entry, widgetFor }) => (
-  <BlogPostTemplate
-    content={widgetFor("body")}
-    description={entry.getIn(["data", "description"])}
-    tags={entry.getIn(["data", "tags"])}
-    title={entry.getIn(["data", "title"])}
-  />
-);
+
+// content,
+//   contentComponent,
+//   description,
+//   tags,
+//   title,
+//   subtitle,
+//   date,
+//   helmet,
+//   videoType,
+//   videoId
+
+const BlogPostPreview = ({ entry, widgetFor }) => {
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+  const date = new Date(widgetFor("date").props.value);
+
+  return (
+    <BlogPostTemplate
+      content={widgetFor("body")}
+      description={entry.getIn(["data", "description"])}
+      tags={entry.getIn(["data", "tags"])}
+      title={entry.getIn(["data", "title"])}
+      subtitle={entry.getIn(["data", "subtitle"])}
+      date={`${monthNames[date.getMonth()]} ${date.getFullYear()}`}
+      videoType={entry.getIn(["data", "videoType"])}
+      videoId={entry.getIn(["data", "videoId"])}
+    />
+  );
+};
 
 BlogPostPreview.propTypes = {
   entry: PropTypes.shape({
