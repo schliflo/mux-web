7075657c5e784721991387791a32e04cc731e005
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2019-08-01 11:43:18 +0200

    fix linting issue

--- src/components/Awards.js
+++ src/components/Awards.js
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes, { array } from "prop-types";
+import PropTypes from "prop-types";
 import { graphql, StaticQuery } from "gatsby";
 import Award from "./Award";
 
